5500d3cd9af2abd80ba0d39be8f9132b84b74a1a
Nick De Breuck <nick.de.breuck@outlook.com>
2019-04-18 21:11:52 +0200

    created main file

---
+++ AbstractedProject/src/main.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "DirectXApp.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int showCmd)
+{
+	//Notify user if heap is corrupt
+	HeapSetInformation(NULL, HeapEnableTerminationOnCorruption, NULL, 0);
+
+	//Enable runtime memory leak check for debug builds.
+#if defined(DEBUG) || defined (_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	typedef HRESULT(__stdcall *fPtr)(const IID&, void**);
+	HMODULE hd12 = LoadLibrary(L"dxgidebug.dll");
+	fPtr DXGIGetDebugInterface = (fPtr)GetProcAddress(hd12, "DXGIGetDebugInterface");
+
+	IDXGIDebug* pDXGIDebug;
+	DXGIGetDebugInterface(__uuidof(IDXGIDebug), (void**)&pDXGIDebug);
+	//_CrtSetBreakAlloc(...);
+#endif
+
+	DirectXApp app(hInstance);
+	return app.Start();
+}
+
+int main()
+{
+	int err = 0;
+	try
+	{
+		err = WinMain(GetModuleHandle(0), 0, 0, SW_SHOW);
+	}
+	catch (Exception& e)
+	{
+		std::wcerr << e.ToString() << std::endl;
+		std::wcin.get();
+		return 0;
+	}
+	
+	return err;
+
+}
