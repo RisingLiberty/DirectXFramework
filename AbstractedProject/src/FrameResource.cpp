e6d4f6cbaac662a0c5554670fcbb9fc8c9bfaddc
Nick De Breuck <nick.debreuck@outlook.com>
2019-04-29 14:44:12 +0200

    created FrameResource class

---
+++ AbstractedProject/src/FrameResource.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "FrameResource.h"
+
+#include "Device.h"
+#include "CommandAllocator.h"
+
+FrameResource::FrameResource(Device* pDevice, unsigned int passCount, unsigned int objectCount, unsigned int materialCount)
+{
+	m_pCommandAllocator = std::make_unique<CommandAllocator>();
+
+	m_pPassConstants = std::make_unique<UploadBuffer<PassConstants>>(pDevice, passCount, true);
+	m_pObjectConstants = std::make_unique<UploadBuffer<ObjectConstants>>(pDevice, objectCount, true);
+	m_pMaterialConstants = std::make_unique<UploadBuffer<MaterialConstants>>(pDevice, materialCount, true);
+}
