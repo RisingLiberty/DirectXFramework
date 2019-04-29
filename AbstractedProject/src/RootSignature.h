6e76042bc54179fbf1e20fbdbe296aae1242f963
Nick De Breuck <nick.debreuck@outlook.com>
2019-04-29 15:25:53 +0200

    adjusted root signature class with helper methods and return methods

--- AbstractedProject/src/RootSignature.h
+++ AbstractedProject/src/RootSignature.h
@@ -1,12 +1,19 @@
 #pragma once
 
 using RootParameter = CD3DX12_ROOT_PARAMETER;
+using RootSignatureDesc = CD3DX12_ROOT_SIGNATURE_DESC;
+
+class Device;
 
 class RootSignature
 {
 public:
-	RootSignature(RootParameter* pParameters, unsigned int nrOfParameters);
+	RootSignature(Device* pDevice, RootParameter* pParameters, unsigned int nrOfParameters);
+
+	ID3D12RootSignature* GetRootSignature() const;
+	ID3D12RootSignature* const* GetRootSignatureAddress() const;
 
 private:
+	ComPtr<ID3D12RootSignature> m_RootSignature;
 
 };
