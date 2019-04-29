f934faed85f075f061adf70d73224682ab67503b
Nick De Breuck <nick.debreuck@outlook.com>
2019-04-29 14:43:43 +0200

    created RootSignature class

---
+++ AbstractedProject/src/RootSignature.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "RootSignature.h"
+
+RootSignature::RootSignature(RootParameter* pParameters, unsigned int nrOfParameters)
+{
+	
+}
---
+++ AbstractedProject/src/RootSignature.h
@@ -0,0 +1,12 @@
+#pragma once
+
+using RootParameter = CD3DX12_ROOT_PARAMETER;
+
+class RootSignature
+{
+public:
+	RootSignature(RootParameter* pParameters, unsigned int nrOfParameters);
+
+private:
+
+};
