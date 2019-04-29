4131184e02a9d02426a6c6ea833127983b6c05bd
Nick De Breuck <nick.de.breuck@outlook.com>
2019-04-29 07:18:09 +0200

    device now creates direct x objects

--- AbstractedProject/src/Device.cpp
+++ AbstractedProject/src/Device.cpp
@@ -3,6 +3,8 @@
 
 #include "Adapter.h"
 
+#include "CommandAllocator.h"
+
 Device::Device(Adapter* pAdapter)
 {
 	ThrowIfFailedDefault(D3D12CreateDevice(pAdapter->GetAdapter(), D3D_FEATURE_LEVEL_12_0, IID_PPV_ARGS(m_Device.ReleaseAndGetAddressOf())));
@@ -15,7 +17,7 @@ Device::~Device()
 
 HRESULT Device::CheckFeatureSupport(D3D12_FEATURE feature, void* featureSupportData, unsigned int size)
 {
-	return m_Device->CheckFeatureSupport(D3D12_FEATURE_MULTISAMPLE_QUALITY_LEVELS, featureSupportData, size);
+	return m_Device->CheckFeatureSupport(feature, featureSupportData, size);
 }
 
 ID3D12Device* Device::GetDevice() const
@@ -26,6 +28,41 @@ ID3D12Device* Device::GetDevice() const
 ID3D12Device* const* Device::GetDeviceAddress() const
 {
 	return m_Device.GetAddressOf();
+}
+
+void Device::CreateCommandAllocator(const D3D12_COMMAND_LIST_TYPE& type, ID3D12CommandAllocator** ppCommandAllocator) const
+{
+	ThrowIfFailedDevice(m_Device->CreateCommandAllocator(type, IID_PPV_ARGS(ppCommandAllocator)));
+}
+
+void Device::CreateCommandList(unsigned int nodeMask, const D3D12_COMMAND_LIST_TYPE& type, CommandAllocator* pAllocator, ID3D12GraphicsCommandList** ppCommandList) const
+{
+	ThrowIfFailedDevice(m_Device->CreateCommandList(nodeMask, type, pAllocator->GetAllocator(), nullptr, IID_PPV_ARGS(ppCommandList)));
+}
+
+void Device::CreateCommandQueue(const D3D12_COMMAND_QUEUE_DESC& desc, ID3D12CommandQueue** ppCommandQueue) const
+{
+	ThrowIfFailedDevice(m_Device->CreateCommandQueue(&desc, IID_PPV_ARGS(ppCommandQueue)));
+}
+
+void Device::CreateFence(unsigned int initialValue, D3D12_FENCE_FLAGS flags, ID3D12Fence** ppFence) const
+{
+	ThrowIfFailedDevice(m_Device->CreateFence(initialValue, flags, IID_PPV_ARGS(ppFence)));
+}
+
+void Device::CreateDescriptorHeap(const DescriptorHeapDesc& desc, ID3D12DescriptorHeap** ppDescriptorHeap) const
+{
+	ThrowIfFailedDevice(m_Device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(ppDescriptorHeap)));
+}
+
+void Device::CreateResource(const CoreResourceDesc& desc, ID3D12Resource** ppResource) const
+{
+	ThrowIfFailedDevice(m_Device->CreateCommittedResource(&desc.Props, desc.Flags, &desc.Desc, desc.States, &desc.ClearValue, IID_PPV_ARGS(ppResource)));
+}
+
+void Device::CreateDepthStencilView(ID3D12Resource* pResource, D3D12_CPU_DESCRIPTOR_HANDLE DestDescriptor, const D3D12_DEPTH_STENCIL_VIEW_DESC* pDesc) const
+{
+	ThrowIfFailedDevice(m_Device->CreateDepthStencilView(pResource, pDesc, DestDescriptor));
 }
 
 UINT Device::GetRenderTargetViewSize() const
--- AbstractedProject/src/Device.h
+++ AbstractedProject/src/Device.h
@@ -1,6 +1,10 @@
 #pragma once
 
 class Adapter;
+class CommandAllocator;
+
+#include "DescriptorHeap.h"
+#include "Texture.h"
 
 class Device
 {
@@ -10,6 +14,14 @@ public:
 
 	ID3D12Device* GetDevice() const;
 	ID3D12Device* const* GetDeviceAddress() const;
+
+	void CreateCommandAllocator(const D3D12_COMMAND_LIST_TYPE& type, ID3D12CommandAllocator** ppCommandAllocator) const;
+	void CreateCommandList(unsigned int nodeMask, const D3D12_COMMAND_LIST_TYPE& type, CommandAllocator* pAllocator, ID3D12GraphicsCommandList** ppCommandList) const;
+	void CreateCommandQueue(const D3D12_COMMAND_QUEUE_DESC& desc, ID3D12CommandQueue** ppCommandQueue) const;
+	void CreateFence(unsigned int initialValue, D3D12_FENCE_FLAGS flags, ID3D12Fence** ppFence) const;
+	void CreateDescriptorHeap(const DescriptorHeapDesc& desc, ID3D12DescriptorHeap** ppDescriptorHeap) const;
+	void CreateResource(const CoreResourceDesc& desc, ID3D12Resource** ppResource) const;
+	void CreateDepthStencilView(ID3D12Resource* pResource, D3D12_CPU_DESCRIPTOR_HANDLE DestDescriptor, const D3D12_DEPTH_STENCIL_VIEW_DESC* pDesc = nullptr) const;
 
 	HRESULT CheckFeatureSupport(D3D12_FEATURE feature, void* featureSupportData, unsigned int size);
 
